aa79b39515dfef4044ac8c7710b6dfb432245311
Kyle Welch <kwelch0626@gmail.com>
2019-06-01 23:17:47 -0700

    make the ticketbot actually work

--- functions/eb-ticketbot.js
+++ functions/eb-ticketbot.js
@@ -1,16 +1,72 @@
 import fetch from 'node-fetch';
+import eventbrite from 'eventbrite';
 require('dotenv').config();
+
+const sdk = eventbrite({
+  baseUrl: '',
+  token: process.env.EB_OAUTH_TOKEN,
+});
+
+const getAttendeeTicketCount = ({ attendees }) =>
+  attendees.map(
+    ({ quantity, ticket_class_name: ticketType }) => `${quantity} ${ticketType}`
+  );
+
+const actionHandlerList = {
+  test: {
+    getMessage: () => `Hello :wave:, the ticketbot is working!`,
+  },
+  'order.placed': {
+    getData: async (url) => sdk.request(`${url}?expand=attendees`),
+    getMessage: (data) =>
+      `:tada::tada::tada:Order placed!\n*Details*:\n${getAttendeeTicketCount(
+        data
+      ).join('\n')}`,
+  },
+};
+
+const returnJson = (obj, overrides) => ({
+  headers: {
+    'content-type': 'application/json',
+  },
+  statusCode: 200,
+  body: JSON.stringify(obj, null, 4),
+  ...overrides,
+});
 
 exports.handler = async (event, context) => {
   // Only allow POST
-  // if (event.httpMethod !== 'POST') {
-  //   return { statusCode: 405, body: 'Method Not Allowed' };
-  // }
+  if (event.httpMethod !== 'POST' && !process.env.OVERRIDE_POST_ONLY) {
+    return returnJson(
+      {
+        message: 'Method Not Allowed',
+      },
+      { statusCode: 405 }
+    );
+  }
 
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a queryString
-  // const params = querystring.parse(event.body);
-  // const name = params.name || "World";
+  const {
+    api_url: apiURL,
+    config: { action },
+  } = JSON.parse(event.body);
+
+  const handler = actionHandlerList[action];
+
+  if (!handler) {
+    return returnJson({
+      message: 'No handler defined for this action.',
+    });
+  }
+
+  let data;
+  let message;
+
+  try {
+    data = handler.getData ? await handler.getData(apiURL) : null;
+    message = handler.getMessage(data);
+  } catch (ex) {
+    return returnJson(ex.parsedError || ex, { statusCode: 500 });
+  }
 
   // Send greeting to Slack
   return fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -18,14 +74,13 @@ exports.handler = async (event, context) => {
       'content-type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify({ text: `test` }),
+    body: JSON.stringify({ text: message }),
   })
-    .then(() => ({
-      statusCode: 200,
-      body: `Message sent`,
-    }))
-    .catch((error) => ({
-      statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`,
-    }));
+    .then(() => returnJson({ message: `Message sent` }))
+    .catch((error) =>
+      returnJson(
+        { message: `Oops! Something went wrong. ${error}` },
+        { statusCode: 422 }
+      )
+    );
 };

16ebb4b20f2bd318a04b63bfa422876d544dc815
Kyle Welch <kwelch0626@gmail.com>
2019-06-05 22:51:14 -0700

    updated to use local images and added XOi tech

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,20 +6,29 @@
 
 module.exports = {
   siteMetadata: {
-    title: "UndergroundJS",
+    title: 'UndergroundJS',
     description:
-      "JavaScript conference hosted in Rocketown in Nashville on August 2nd 2019.",
-    twitter: "https://twitter.com/undergroundjs",
-    instagram: "",
+      'JavaScript conference hosted in Rocketown in Nashville on August 2nd 2019.',
+    twitter: 'https://twitter.com/undergroundjs',
+    instagram: '',
   },
-  __experimentalThemes: ["gatsby-theme-conference"],
+  __experimentalThemes: ['gatsby-theme-conference'],
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images',
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-138176222-1",
+        trackingId: 'UA-138176222-1',
       },
     },
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
--- src/gatsby-theme-conference/components/sponsors.js
+++ src/gatsby-theme-conference/components/sponsors.js
@@ -1,43 +1,66 @@
 import React from 'react';
-import { Styled, css } from 'theme-ui'
-import { Box, Container } from 'theme-ui/layout'
-import SectionHeading from '../../../node_modules/gatsby-theme-conference/src/components/section-heading'
-import FlexList from '../../../node_modules/gatsby-theme-conference/src/components/flex-list';
-import Card from '../../../node_modules/gatsby-theme-conference/src/components/card';
+import { Styled, css } from 'theme-ui';
+import { Box, Container } from 'theme-ui/layout';
+import SectionHeading from 'gatsby-theme-conference/src/components/section-heading';
+import FlexList from 'gatsby-theme-conference/src/components/flex-list';
+import Card from 'gatsby-theme-conference/src/components/card';
+import { useStaticQuery } from 'gatsby';
 
-export default ({ sponsors = [] }) =>
-  <Box
-    id='sponsors'
-    py={5}
-    css={css({
-      display: "flex",
-      flexDirection: "column",
-    })}>
-    <Container>
-      <SectionHeading>
-        Sponsors
-      </SectionHeading>
-      <FlexList
-        css={{
-          justifyContent: 'center',
-        }}>
-        {sponsors.map((sponsor, i) => (
-          <Card as='li' key={sponsor.id}>
-            <Styled.a
-              title={sponsor.name}
-              href={sponsor.url}>
+export default ({ sponsors = [] }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: { extension: { eq: "svg" } }) {
+        nodes {
+          name
+          publicURL
+        }
+      }
+    }
+  `);
+
+  const {
+    allFile: { nodes: images },
+  } = data;
+
+  console.log(data, images);
+
+  return (
+    <Box
+      id="sponsors"
+      py={5}
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+      })}
+    >
+      <Container>
+        <SectionHeading>Sponsors</SectionHeading>
+        <FlexList
+          css={{
+            justifyContent: 'center',
+          }}
+        >
+          {sponsors.map((sponsor, i) => (
+            <Card as="li" key={sponsor.id}>
+              <Styled.a title={sponsor.name} href={sponsor.url}>
                 <Styled.img
-                  src={sponsor.logo}
+                  src={
+                    images.find(({ name }) => name === sponsor.logo).publicURL
+                  }
+                  alt={sponsor.name}
                   css={css({
-                    width: i === 0 ? [360, 600, 740, 920] : [250, 414, 510, 635],
+                    width:
+                      i === 0 ? [360, 600, 740, 920] : [250, 414, 510, 635],
                     mb: 2,
-                    mx: "auto",
-                    alignSelf: "center",
+                    mx: 'auto',
+                    alignSelf: 'center',
                   })}
                 />
-            </Styled.a>
-          </Card>
-        ))}
-      </FlexList>
-    </Container>
-  </Box>
+              </Styled.a>
+            </Card>
+          ))}
+        </FlexList>
+      </Container>
+    </Box>
+  );
+};

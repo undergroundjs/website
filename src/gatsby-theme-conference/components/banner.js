31df8e210535489ccd1d283420ac2916b6ab3f4a
Kyle Welch <kwelch0626@gmail.com>
2019-07-10 08:34:01 -0500

    Add scholarship form to website (#15)
    * Add scholarship form to website
    
    * Update banner.js

--- src/gatsby-theme-conference/components/banner.js
+++ src/gatsby-theme-conference/components/banner.js
@@ -1,43 +1,47 @@
-import React from "react"
-import { Styled, css } from "theme-ui"
-import { Box, Container } from "theme-ui/layout"
-import useSiteMetadata from "gatsby-theme-conference/src/use-site-metadata"
-import Button from "gatsby-theme-conference/src/components/button"
+import React from 'react';
+import { Styled, css } from 'theme-ui';
+import { Box, Container } from 'theme-ui/layout';
+import useSiteMetadata from 'gatsby-theme-conference/src/use-site-metadata';
+import Button from 'gatsby-theme-conference/src/components/button';
 
-export default props => {
-  const { description } = useSiteMetadata()
+export default (props) => {
+  const { description } = useSiteMetadata();
 
   return (
     <Box {...props} py={[5, 6]}>
       <Container
         css={css({
-          display: "flex",
-          flexDirection: "column",
-        })}>
+          display: 'flex',
+          flexDirection: 'column',
+        })}
+      >
         <Styled.img
           css={css({
             width: [375, 600, 740, 920],
             mb: 2,
-            mx: "auto",
-            alignSelf: "center",
+            mx: 'auto',
+            alignSelf: 'center',
           })}
-          src={require("../../images/logo-3d.png")}
+          src={require('../../images/logo-3d.png')}
         />
         <Styled.p
           css={css({
-            textAlign: "center",
+            textAlign: 'center',
             fontSize: 3,
             mb: 5,
           })}
         >
           {description}
         </Styled.p>
-        <div css={{ display: "flex", justifyContent: "space-around" }}>
+        <div css={{ display: 'flex', justifyContent: 'space-around' }}>
           <Button href="https://www.eventbrite.com/e/undergroundjs-tickets-61081666910?aff=website">
             Buy Tickets
+          </Button>
+          <Button href="https://forms.gle/8W92EGiu66pwUxCM7">
+            Scholarship
           </Button>
         </div>
       </Container>
     </Box>
-  )
-}
+  );
+};

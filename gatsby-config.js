e02c3487fca23957f0b3eb94b388d97a82a07447
Kyle Welch <kwelch0626@gmail.com>
2019-07-30 21:14:56 -0500

    Add offline support (#25)

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };

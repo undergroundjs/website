c79cf36a8b3a6067ae929c695e755c6e6a108de5
Hellrungj <johnjameshellrung@gmail.com>
2020-03-04 19:35:54 -0600

    Added React-bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Card, Carousel, Button } from 'react-bootstrap'
 import Layout from '../components/layout'
 import '../styles/index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default () => {
   return (
@@ -21,6 +23,52 @@ export default () => {
               </a>
           </div>
       </div>
+      <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/800x400.jpeg/?text=First slide&bg=373940"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/800x400.jpeg/?text=Second slide&bg=373940"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/800x400.jpeg/?text=Third slide&bg=373940"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://via.placeholder.com/300x200.jpeg/55595c/ffffff/?text=Thumbnail" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
     </Layout>
   );
 };

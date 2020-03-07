100c0bcafb2d39c634834c4dfb5f86664ec0c128
Hellrungj <johnjameshellrung@gmail.com>
2020-03-07 08:35:45 -0600

    Add new pages Add the following pages: - contact - schedule - speakers

---
+++ src/pages/schedule.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+export default () => {
+  return (
+      <Layout>
+          <h1>Schedule Page</h1>
+      </Layout>
+  )
+}

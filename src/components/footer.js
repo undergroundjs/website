597bac79ff2e583db3d91ca383d696284224740f
scottsilver <scottsilver47@gmail.com>
2020-03-15 18:44:05 -0500

    added links for privacy pollicy and terms of service

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,53 +1,65 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'gatsby';
 import { Twitter } from 'react-feather';
 
 const Footer = () => {
-    return (
-        <footer class="footer">
-            <div class='footerMenu'>
-                <div class="archive">
-                    <ul>
-                        <a href="/2019" component={Link} to="/2019">
-                            2019 Archive
-                        </a>
-                    </ul>
-                </div>
-                <div class="pages">
-                    <ul>
-                        <a href="/faq" component={Link} to="/faq">
-                            FAQ
-                        </a>
-                    </ul>
-                    <ul>
-                        <a href="/about" component={Link} to="/about">
-                            About
-                        </a>
-                    </ul>
-                    <ul>
-                        <a href="/code-of-conduct" component={Link} to="/code-of-conduct">
-                            Code of Conduct
-                        </a>
-                    </ul>
-                    <ul>
-                        <a href="/become-a-sponsor" component={Link} to="/become-a-sponsor">
-                            Become a Sponsor
-                        </a>
-                    </ul>
-                </div>
-                <div class="social">
-                    <ul>
-                        <a href="https://twitter.com/undergroundjs" component={Link} to="https://twitter.com/undergroundjs">
-                            <Twitter />
-                        </a>
-                    </ul>
-                </div>
-            </div>
-            <p class="copyright">
-                © 2020 UndergroundJS, Inc.
-            </p>
-        </footer>
-    )
-}
+  return (
+    <footer class="footer">
+      <div class="footerMenu">
+        <div class="archive">
+          <ul>
+            <a href="/2019" component={Link} to="/2019">
+              2019 Archive
+            </a>
+          </ul>
+        </div>
+        <div class="pages">
+          <ul>
+            <a href="/become-a-sponsor" component={Link} to="/become-a-sponsor">
+              Become a Sponsor
+            </a>
+          </ul>
+          <ul>
+            <a href="/faq" component={Link} to="/faq">
+              FAQ
+            </a>
+          </ul>
+          <ul>
+            <a href="/about" component={Link} to="/about">
+              About
+            </a>
+          </ul>
+          <ul>
+            <a href="/code-of-conduct" component={Link} to="/code-of-conduct">
+              Code of Conduct
+            </a>
+          </ul>
+          <ul>
+            <a href="/privacy-policy" component={Link} to="/privacy-policy">
+              Privacy Policy
+            </a>
+          </ul>
+          <ul>
+            <a href="/terms-of-service" component={Link} to="/terms-of-service">
+              Terms of Service
+            </a>
+          </ul>
+        </div>
+        <div class="social">
+          <ul>
+            <a
+              href="https://twitter.com/undergroundjs"
+              component={Link}
+              to="https://twitter.com/undergroundjs"
+            >
+              <Twitter />
+            </a>
+          </ul>
+        </div>
+      </div>
+      <p class="copyright">© 2020 UndergroundJS, Inc.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;

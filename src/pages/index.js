a71557e79f2a2777aa02601b8173b2efb7d1b48d
Seth <setha@hey.com>
2021-01-20 23:24:08 -0600

    remove 2020 info from site

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,44 +32,6 @@ export default (props) => {
           >
             JavaScript conference hosted in Nashville.
           </Styled.h1>
-          <Styled.h3
-            css={css({
-              textAlign: 'center',
-              color: 'background',
-              textShadow: '0px 0px 5px black',
-            })}
-          >
-            2020 details coming soon!
-          </Styled.h3>
-        </Container>
-      </Box>
-      <Box>
-        <Container>
-          <Styled.h3
-            css={css({
-              textAlign: 'center',
-            })}
-          >
-            <Styled.a
-              title="Call for Speakers"
-              href="https://sessionize.com/ugjs-2020"
-            >
-              CFP Now Open
-            </Styled.a>
-          </Styled.h3>
-
-          <Styled.h3
-            css={css({
-              textAlign: 'center',
-            })}
-          >
-            <Styled.a
-              title="Tickets"
-              href="https://www.eventbrite.com/e/undergroundjs-2020-tickets-93439517111?aff=website"
-            >
-              Buy Tickets
-            </Styled.a>
-          </Styled.h3>
         </Container>
       </Box>
     </Layout>
